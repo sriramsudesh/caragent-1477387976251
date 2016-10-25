d2301ff0b6bdc18cc4212510e2c1754afc6e9f90
sudesh@sg.ibm.com <sudesh@sg.ibm.com>
2016-10-25 17:44:57 +0800

    Added conversation service and source code

--- app.js
+++ app.js
@@ -1,28 +1,87 @@
-/*eslint-env node*/
+/*************************************************************************************************
 
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
+  Define global variables for NPM packages and Cloud Foundry environment
 
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
+*************************************************************************************************/
+"use strict";
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
+var express = require('express'),
+    cfenv = require("cfenv"),
+    appEnv = cfenv.getAppEnv(),
+    app = express(),
+    bodyParser = require('body-parser'),
 
-// create a new express server
-var app = express();
+    watson = require('watson-developer-cloud');
 
-// serve the files out of ./public as our main files
+/************************************************************************************************* 
+  
+  Start the server 
+  
+*************************************************************************************************/
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
 app.use(express.static(__dirname + '/public'));
-
-// get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
-
-// start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+    console.log("server starting on " + appEnv.url);
 });
+
+/*************************************************************************************************
+
+ Watson Conversation
+
+*************************************************************************************************/
+var conversation = watson.conversation({
+  "url": "https://gateway.watsonplatform.net/conversation/api",
+  "password": "0g3rxXsTJR43",
+  "username": "2b9c4433-1d2d-4fee-8759-a5cb12672816",
+   "version_date": '2016-07-11',
+   "version": 'v1'
+});
+
+// Allow clients to interact with the bot
+app.post('/api/bot', function(req, res) {
+    
+    console.log("Got request for Le Bot");
+    console.log("Request is: ",req);
+
+    var workspace = 'b7823d36-a6c6-49bb-a60e-a0b76669027b'; // Set to your Conversation workspace ID
+
+    if (!workspace) {
+        console.log("No workspace detected. Cannot run the Watson Conversation service.");
+    }
+
+    var params = {
+        workspace_id: workspace,
+        context: {}, // Null context indicates new conversation
+        input: {}    // Holder for message
+    };
+
+    // Update options to send to conversation service with the user input and a context if one exists
+    if (req.body) {
+        if (req.body.input) {
+            params.input = req.body.input;
+        }
+
+        if (req.body.context) {
+            params.context = req.body.context;
+        }
+    }
+
+    // Send message to the conversation service with the current context
+    conversation.message(params, function(err, data) {
+        if (err) {
+            console.log("Error in sending message: ", err);
+            return res.status(err.code || 500).json(err);
+        }
+
+        console.log("Response: ", data);
+
+        return res.json(data);
+    });
+
+}); // End app.post '/api/bot'
+
