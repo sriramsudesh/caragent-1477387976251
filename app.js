349ff180dfd5cfe364d0b0a71c814d242fe657b7
sudesh@sg.ibm.com <sudesh@sg.ibm.com>
2017-02-20 21:24:06 +0800

    added the code Please enter the commit message for your changes. Lines starting

--- app.js
+++ app.js
@@ -1,87 +1,28 @@
-/*************************************************************************************************
 
-  Define global variables for NPM packages and Cloud Foundry environment
 
-*************************************************************************************************/
-"use strict";
 
-var express = require('express'),
-    cfenv = require("cfenv"),
-    appEnv = cfenv.getAppEnv(),
-    app = express(),
-    bodyParser = require('body-parser'),
 
-    watson = require('watson-developer-cloud');
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-/************************************************************************************************* 
-  
-  Start the server 
-  
-*************************************************************************************************/
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
+const appSecret =  "98ad4368d5bfedd2fec8f64bbe24875f" ;
+const token = "EAAKQ4NzIUlgBAFyEFoShaHVM2SR04TKEljf4xDZCcEb9T0yjQVhuwAG3T4CjiAEGQ9EyyVTZCSbLgmkI2cN1eaWciu9mfDmUzJwYn2usrmmRPGi4fXq6VrnqBHQ03JOQQEpTwxaDET0lgPTSfHK8TePpw5TkvwvFiXCHqFxAZDZD";
 
-app.use(express.static(__dirname + '/public'));
-var appEnv = cfenv.getAppEnv();
-app.listen(appEnv.port, '0.0.0.0', function() {
-    console.log("server starting on " + appEnv.url);
-});
+const validationToken= "facebookpassword";
+const serverURL= "https://www.facebook.com/Askmeanything-213004629169154/";
+//var token = "Replace with the access token from Facebook";
 
-/*************************************************************************************************
-
- Watson Conversation
-
-*************************************************************************************************/
-var conversation = watson.conversation({
-  "url": "https://gateway.watsonplatform.net/conversation/api",
-  "password": "0g3rxXsTJR43",
-  "username": "2b9c4433-1d2d-4fee-8759-a5cb12672816",
-   "version_date": '2016-07-11',
-   "version": 'v1'
-});
-
-// Allow clients to interact with the bot
-app.post('/api/bot', function(req, res) {
     
-    console.log("Got request for Le Bot");
-    console.log("Request is: ",req);
 
-    var workspace = 'b7823d36-a6c6-49bb-a60e-a0b76669027b'; // Set to your Conversation workspace ID
-
-    if (!workspace) {
-        console.log("No workspace detected. Cannot run the Watson Conversation service.");
+// This code is called only when subscribing the webhook //
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === validationToken) {
+        res.send(req.query['hub.challenge']);
     }
-
-    var params = {
-        workspace_id: workspace,
-        context: {}, // Null context indicates new conversation
-        input: {}    // Holder for message
-    };
-
-    // Update options to send to conversation service with the user input and a context if one exists
-    if (req.body) {
-        if (req.body.input) {
-            params.input = req.body.input;
-        }
-
-        if (req.body.context) {
-            params.context = req.body.context;
-        }
-    }
-
-    // Send message to the conversation service with the current context
-    conversation.message(params, function(err, data) {
-        if (err) {
-            console.log("Error in sending message: ", err);
-            return res.status(err.code || 500).json(err);
-        }
-
-        console.log("Response: ", data);
-
-        return res.json(data);
-    });
-
-}); // End app.post '/api/bot'
+    res.send('Error, wrong validation token');
+})
 
