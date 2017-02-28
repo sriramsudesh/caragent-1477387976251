f3ade4e072948f82d8d05199e364b632c076f5b2
sudesh@sg.ibm.com <sudesh@sg.ibm.com>
2017-02-28 14:33:38 +0800

    modified app.js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+var Botkit = require('botkit');
+
 
 const appSecret =  "d13cb86dd7ec1627cbe8a9c714a7aec6" ;
 const token = "EAAS41pkZC8EoBALpHXm4riF19mgxmkuHKAz0yfwi0ooNjaDzGGJeuPQX1ryFozjLZCTv0oVADmEJoVmj8gOi3sVFwaSR0YwyuJlttWgYwYppl01naEU56xMTMFhErc8mfcTnL0BGpGiJMOTy8FtKguZCjZBZCy92ET6aq9bmuqgZDZD";
@@ -28,11 +30,48 @@ app.get('/webhook/', function (req, res) {
 
 app.get('/', function (req, res) {
     
-    res.send('facebook webhook setup !');
+    res.send('Facebook messenger application. This is a Webhook setup !');
 })
 
 var host = (process.env.VCAP_APP_HOST || 'localhost');
 var port = (process.env.VCAP_APP_PORT || 3000);
 app.listen(port,host, function() {
     console.log("server starting on" + "http://localhost:3000");
+});
+
+
+
+const watson = require('watson-developer-cloud');
+
+/**
+ * Instantiate the Watson Conversation Service
+ */
+const conversation = new watson.ConversationV1({
+  username: process.env.CONVERSATION_USERNAME || '27b64514-3f7d-41b0-a63b-5cc6fcf62af4',
+  password: process.env.CONVERSATION_PASSWORD || '4XSICbtLfm7Y',
+  version_date: '2016-07-11'
+});
+
+
+/**
+ * Payload for the Watson Conversation Service
+ * <workspace-id> and user input text required.
+ */
+const payload = {
+  workspace_id: process.env.WORKSPACE_ID || '886e4044-1e18-4424-827b-295ca975478c',
+  input: {
+    text: 'I am happy to talk to you today.'
+  }
+};
+
+conversation.message(payload, function(err, data) {
+  if (err) {
+    // APPLICATION-SPECIFIC CODE TO PROCESS THE ERROR
+    // FROM CONVERSATION SERVICE
+    console.error(JSON.stringify(err, null, 2));
+  } else {
+    // APPLICATION-SPECIFIC CODE TO PROCESS THE DATA
+    // FROM CONVERSATION SERVICE
+    console.log(JSON.stringify(data, null, 2));
+  }
 });
